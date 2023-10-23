cc7150bfac87fbb79e801b6353f112039f860fe1
DarkDrinker <Anders.wade.jensen@hotmail.com>
2023-10-23 10:40:37 +0200

    Start

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,28 @@
+package app;
+import app.config.ThymeleafConfig;
+import app.controllers.UserController;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+public class Main {
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "fourthingsplus";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    public static void main(String[] args) {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            JavalinThymeleaf.init(ThymeleafConfig.templateEngine());
+        }).start(7070);
+
+        // Routing
+
+        app.get("/", ctx -> ctx.render("index.html"));
+        app.post("/login", ctx -> UserController.login(ctx ,connectionPool) );
+    }
+}
---
+++ src/main/java/app/persistence/UserMapper.java
@@ -0,0 +1,35 @@
+package app.persistence;
+
+import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper {
+    public static User login(String name, String password, ConnectionPool connectionPool) throws DatabaseException{
+       String sql = "select * from \"user\" where name=? and password=?";
+
+       try(Connection connection = connectionPool.getConnection())
+       {
+           try(PreparedStatement ps = connection.prepareStatement(sql))
+           {
+               ps.setString(1,name);
+               ps.setString(2,password);
+               ResultSet rs = ps.executeQuery();
+               if(rs.next()){
+                   int id = rs.getInt("id");
+                   return new User(id,name, password);
+               } else {
+                throw new DatabaseException("Login er desværre forkert");
+               }
+           }
+       } catch (SQLException e) {
+           throw new RuntimeException(e);
+       }
+
+
+    }
+}
