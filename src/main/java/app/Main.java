d53be0c1fd9ea4e7bf5f862da710b25dfa7be183
PejterNico <124774197+PejterNico@users.noreply.github.com>
2023-10-25 11:41:52 +0200

    Login and create user / DP connected

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,18 +1,24 @@
 package app;
+
 import app.config.ThymeleafConfig;
+import app.controllers.TaskController;
 import app.controllers.UserController;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
-public class Main {
+
+public class Main
+{
+
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "fourthingsplus";
+    private static final String DB = "CupcakeProjectdb";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // Initializing Javalin and Jetty webserver
 
         Javalin app = Javalin.create(config -> {
@@ -24,5 +30,7 @@ public class Main {
 
         app.get("/", ctx -> ctx.render("index.html"));
         app.post("/login", ctx -> UserController.login(ctx ,connectionPool) );
+        app.get("/createuser", ctx -> ctx.render("createuser.html"));
+        app.post("/createuser",ctx -> UserController.createuser(ctx, connectionPool ));
     }
 }
