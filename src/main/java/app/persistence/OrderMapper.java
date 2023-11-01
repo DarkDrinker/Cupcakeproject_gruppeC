f57bf27a63325a2b3dc5d7b1c5b642e9583ec024
DarkDrinker <Anders.wade.jensen@hotmail.com>
2023-11-01 20:25:36 +0100

    last last

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -12,33 +12,27 @@ import java.util.List;
 
 public class OrderMapper {
 
-    public static List<Order> getAllOrdersPerUser(int user_id, ConnectionPool connectionPool) throws DatabaseException
-    {
+    public static List<Order> getAllOrdersPerUser(int userid, ConnectionPool connectionPool) throws DatabaseException {
         List<Order> orderList = new ArrayList<>();
         String sql = "select * from Orders where user_id=? order by id";
-
-        try (Connection connection = connectionPool.getConnection())
-        {
-            try (PreparedStatement ps = connection.prepareStatement(sql))
-            {
-                ps.setInt(1, user_id);
-                ResultSet rs = ps.executeQuery();
-                while (rs.next())
-                {
-                    int id = rs.getInt("id");
-                    String date = rs.getString("date");
-                    String status = rs.getString("status");
-                    orderList.add(new Order(id, date, status, user_id));
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                    ps.setInt(1, userid);
+                    ResultSet rs = ps.executeQuery();
+                    while (rs.next()) {
+                        int id = rs.getInt("id");
+                        String date = rs.getString("date");
+                        String status = rs.getString("status");
+                        orderList.add(new Order(id, date, status, userid));
+                    }
                 }
-            }
 
+            } catch (SQLException e) {
+                throw new DatabaseException("Fejl i Orders");
+            }
+            return orderList;
         }
-        catch (SQLException e)
-        {
-            throw new DatabaseException("Fejl i Orders");
-        }
-        return orderList;
-    }
+
 
 
     public static void addOrder(int id,String date, String status, int userID, ConnectionPool connectionPool){
